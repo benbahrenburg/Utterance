b0a97d1da258a402fc25cabb1e452894170cd8ec
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2014-09-07 12:46:51 -0700

    PoC for Installing TTS data

--- android/src/bencoding/utterance/InstallTTSDataProxy.java
+++ android/src/bencoding/utterance/InstallTTSDataProxy.java
@@ -1,5 +1,6 @@
 package bencoding.utterance;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.appcelerator.kroll.KrollDict;
@@ -13,21 +14,36 @@ import org.appcelerator.titanium.util.TiActivitySupport;
 import android.app.Activity;
 import android.content.Intent;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 
 @Kroll.proxy(creatableInModule=UtteranceModule.class)
 public class InstallTTSDataProxy extends KrollProxy implements TiActivityResultHandler, KrollProxyListener {
 
-	private static final int CHECK_INTENT_ID = 44400;
+	private static final int CHECK_INSTALL_INTENT_ID = 44400;
 	private static final int INSTALL_INTENT_ID = 54400;
+	private static final String EVENT_INSTALL_CHECK = "installcheck";
 	
 	public InstallTTSDataProxy(){		
 		super();
+	}
+
+	@Kroll.method
+	public void CheckDataInstalled()
+	{
 		Intent checkTTSIntent = new Intent();
 		checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
 		final TiActivitySupport activitySupport = (TiActivitySupport) activity;
-		activitySupport.launchActivityForResult(checkTTSIntent, CHECK_INTENT_ID, this);		
+		activitySupport.launchActivityForResult(checkTTSIntent, CHECK_INSTALL_INTENT_ID, this);		
 	}
 
+	@Kroll.method
+	public void InstallData()
+	{
+		Intent checkTTSIntent = new Intent();
+		checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+		final TiActivitySupport activitySupport = (TiActivitySupport) activity;
+		activitySupport.launchActivityForResult(checkTTSIntent, INSTALL_INTENT_ID, this);		
+	}
 	
 	@Override
 	public void listenerAdded(String arg0, int arg1, KrollProxy arg2) {
@@ -49,29 +65,46 @@ public class InstallTTSDataProxy extends KrollProxy implements TiActivityResultH
 	public void propertyChanged(String arg0, Object arg1, Object arg2, KrollProxy arg3) {}
 
 	@Override
-	public void onError(Activity arg0, int arg1, Exception arg2) {	
-		
+	public void onError(Activity activity, int requestCode, Exception error) {	
+		Log.d(UtteranceModule.MODULE_FULL_NAME, "onError : Errored");
+		if (requestCode == CHECK_INSTALL_INTENT_ID) {
+			if (hasListeners(EVENT_INSTALL_CHECK)) {
+				HashMap<String, Object> event = new HashMap<String, Object>();
+				event.put("success",false);
+				event.put("requestCode",requestCode);
+				event.put("message",error.getMessage());
+				fireEvent(EVENT_INSTALL_CHECK, event);
+				Log.d(UtteranceModule.MODULE_FULL_NAME,"event: " + EVENT_INSTALL_CHECK+ " fired");
+			}else{
+				Log.d(UtteranceModule.MODULE_FULL_NAME,"event: " + EVENT_INSTALL_CHECK+ " not found");
+			}  			
+		}
 	}
 
 	@Override
 	public void onResult(Activity activity, int requestCode, int resultCode, Intent data) {
-	    if (requestCode == CHECK_INTENT_ID) {
-	        if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {      
-	        	//Success
-	        }
-	        else {
-	            Intent installTTSIntent = new Intent();
-	            installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
-	    		final TiActivitySupport activitySupport = (TiActivitySupport) activity;
-	    		activitySupport.launchActivityForResult(installTTSIntent, INSTALL_INTENT_ID, this);	
-	        }
-	    }
-	    
-//	    if (requestCode == INSTALL_INTENT_ID) {
-//	        if (resultCode == TextToSpeech.Engine.) {      
-//
-//	        }	    	
-//	    }
+		Log.d(UtteranceModule.MODULE_FULL_NAME, "onResult : onResult");
+		Log.d(UtteranceModule.MODULE_FULL_NAME, "onResult : requestCode" + requestCode);
+		
+		try{
+		    if (requestCode == CHECK_INSTALL_INTENT_ID) {		    	
+				if (hasListeners(EVENT_INSTALL_CHECK)) {
+					String msg = (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) ? "TTS Data Installed" : "TTS Data Not installed";
+					HashMap<String, Object> event = new HashMap<String, Object>();
+					event.put("success",(resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) );
+					event.put("requestCode",requestCode);
+					event.put("message",msg);
+					fireEvent(EVENT_INSTALL_CHECK, event);
+					Log.d(UtteranceModule.MODULE_FULL_NAME,"event: " + EVENT_INSTALL_CHECK+ " fired");
+				}else{
+					Log.d(UtteranceModule.MODULE_FULL_NAME,"event: " + EVENT_INSTALL_CHECK+ " not found");
+				}				
+		    }
+		    
+		}catch(Exception error){
+			Log.e(UtteranceModule.MODULE_FULL_NAME, error.getMessage());
+			error.printStackTrace();
+		}
 	}
 
 }
